bb03fe5f47e6f97f5d5c24c4632ccde419726710
mahdizaferanchi <mahdizaferanchi@gmail.com>
2022-05-09 06:18:19 +0000

    testing fusing with fused function which made no improvement

--- read_to_numpy.py
+++ read_to_numpy.py
@@ -5,27 +5,27 @@ win_out_pooled = np.fromfile('tensors/winograd_out_pooled.bin', dtype=np.single)
 cudnnout = np.fromfile('tensors/cudnnout.bin', dtype=np.single).reshape((14, 14, 128)).transpose((2, 0, 1))
 pooled = np.fromfile('tensors/pooled.bin', dtype=np.single).reshape((7, 7, 128)).transpose((2, 0, 1))
 
-# print('win out')
-# print(win_out[0])
-# print(win_out.shape)
-# print('')
-
-print('win out pooled')
-print(win_out_pooled[0])
-# print(win_out_pooled[66][1:-1, 1:-1])
-print(win_out_pooled.shape)
+print('win out')
+print(win_out[0][1:-1, 1:-1])
+print(win_out.shape)
 print('')
 
-# print('cudnn out')
-# print(cudnnout[0][:4])
-# print(cudnnout.shape)
+# print('win out pooled')
+# print(win_out_pooled[0])
+# # print(win_out_pooled[66][1:-1, 1:-1])
+# print(win_out_pooled.shape)
 # print('')
 
-print('cudnn out pooled')
-print(pooled[0])
-# print(pooled[66])
-print(pooled.shape)
+print('cudnn out')
+print(cudnnout[0][:4])
+print(cudnnout.shape)
 print('')
+
+# print('cudnn out pooled')
+# print(pooled[0])
+# # print(pooled[66])
+# print(pooled.shape)
+# print('')
 
 
 At = np.array([[ 1, 1, 1, 1, 1, 0],
